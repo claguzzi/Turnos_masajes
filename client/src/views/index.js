150d3de8581eb8b904ce1719af6e741ea73abd88
DESKTOP-BMQPELL\claudio <claudiolaguzzi@gmail.com>
2026-02-04 01:09:33 -0300

    first commit

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -3,10 +3,12 @@ import Landing from "./Landing"
 import Admin from "./Admin"
 import AdminLogin from "./AdminLogin"
 import Success from "./Success"
+import Failure from "./Failure"
 
 
 
 
 
-export { Home, Landing, AdminLogin, Admin, Success }
+
+export { Home, Landing, AdminLogin, Admin, Success, Failure }
 
